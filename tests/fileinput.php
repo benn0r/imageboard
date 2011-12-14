ed2c6f4d0447f581b5beff0138928bc2676b0b17
benn0r <admin@benn0r.ch>
2011-12-14 20:53:53 +0100

    new test folder

---
+++ tests/fileinput.php
@@ -0,0 +1,13 @@
+<!-- required scripts -->
+<script type="text/javascript" src="enhance.js"></script>
+<script type="text/javascript" src="../javascripts/jquery.js"></script>
+<script type="text/javascript" src="../javascripts/jquery/fileinput.js"></script>
+
+<!-- required stylehsheets -->
+<link rel="stylesheet" type="text/css" href="../styles/jquery/fileinput.css" />
+
+<input type="file" name="file" id="file" />
+
+<script type="text/javascript">
+	$('#file').customFileInput();
+</script>
