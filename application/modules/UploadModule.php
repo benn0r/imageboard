ba1efd8c50b744f4bdbeae8d3c66349288ff7f63
benn0r <admin@benn0r.ch>
2011-12-18 13:57:48 +0100

    fixed date

--- application/modules/UploadModule.php
+++ application/modules/UploadModule.php
@@ -7,7 +7,7 @@
  * 
  * @author benn0r <benjamin@benn0r.ch>
  * @since 2011/11/02
- * @version 2011/12/14
+ * @version 2011/12/18
  */
 class UploadModule extends Module
 {
@@ -15,7 +15,6 @@ class UploadModule extends Module
 	public function run(array $args) {
 		if (isset($args[1])) switch ($args[1]) {
 			case 'localfile':
-				
 				print_r($_FILES);
 				break;
 		}
