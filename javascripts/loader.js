f6c099dcca5bd98a8d24f767ea3cb2bc93a5fd78
benn0r <admin@benn0r.ch>
2012-01-20 21:10:00 +0100

    cleanedup javascript and made some comments

---
+++ javascripts/loader.js
@@ -0,0 +1,97 @@
+/*!
+ * called from onclick event of links or buttons
+ * 
+ * @param link string
+ * @returns false
+ */
+function loadpage(link) {
+	return loadlink($(link).attr('href'));
+}
+
+/*!
+ * called with a link
+ * 
+ * @param uri link
+ * @returns false
+ */
+function loadlink(uri) {
+	History.pushState({rand: Math.random()}, $(document).attr('title'), uri);
+	return false;
+}
+
+/*!
+ * 
+ * 
+ * @param uri string
+ * @param data serialized data for post
+ * @returns
+ */
+function postlink(uri, data) {			
+	var link = uri.replace(BASE_URL, '');
+
+	lastlink = link;
+
+	if (typeof history.pushState != 'undefined') {
+		history.pushState({foo: 'bar'}, uri, uri);
+	} else {
+		document.location.href = '#!' + link;
+	}
+
+	if (uri.indexOf('?') > 0) {
+		uri = uri + '&ajax=1';
+	} else {
+		uri = uri + '/?ajax=1';
+	}
+
+	$.post(uri, data, function(result) {
+		loadlink(result);
+	});
+
+	return false;
+}
+function submitform(form, target) {
+	var form = $(form);
+
+	if (form.attr('method') == 'post') {
+		$.post(form.attr('action'), form.serialize(), function(result) {
+			target.html(result);
+		});
+	}
+
+	// onsubmit cancel
+	return false;
+}
+
+/*!
+ * submits login form and checks if login is correct
+ * 
+ * @param form
+ * @returns
+ */
+function submitlogin(form) {
+	var form = $(form);
+
+	if (form.find('input[type=text][value=]').size() > 0) {
+		form.find('input[type=text][value=]').first().select();
+		return false;
+	}
+
+	form.find('button').button('loading');
+	form.find('.alert-message').remove();
+
+	$.post(BASE_URL + 'login', $(form).serialize(), function(result) {
+		if (result == 0) {
+			$('#modal-loginerror').modal('show');
+
+			// clear loginform
+			form.find('input').val('');
+
+			// reset submit button state
+			form.find('button').button('reset');
+		} else {
+			// login successful
+			document.location = document.location;
+		}
+	});
+	return false;
+}
--- javascripts/notifications.js
+++ javascripts/notifications.js
@@ -1,29 +1,31 @@
-/**
+/*!
  * storage for last notification id
  * 
  * @var int
  */
 var lastn = 0;
 
-/**
+/*!
  * called every x seconds to check for new notifications
  */
 var notificationsTimer = setInterval(notifications = function notifications() {
 	$.getJSON(BASE_URL + 'notifications/?nid=' + lastn, function(data) {
-
+		
 		if (data.users.length == 0) {
+			// sorry, no active users
 			$('.mainpage-users-container').hide();
 		} else {
 			$('.mainpage-users-container').show();
 			$('.mainpage-users').html('');
 			
+			// add online users
 			for (var i = 0; i < data.users.length; i++) {
 				var u = data.users[i];
-				
 				$('.mainpage-users').append('<a onclick="return loadpage(this)" title="' + u.username + '" href="' + BASE_URL + 'user/' + u.uid + '/"><img src="' + u.avatar + '"</a>');
 			}
 		}
 
+		// fetch notifications
 		for (var i = 0; i < data.notifications.length; i++) {
 			var n = data.notifications[i];
 
@@ -52,70 +54,79 @@ var notificationsTimer = setInterval(notifications = function notifications() {
 		notificationRenderer();
 		updateNotificationsCount();
 
-		/*for (var i = 0; i < data.news.length; i++) {
-			var n = data.news[i];
-
-			var elem = $('<li><h5>' + n.inserttime + '</h5>' + n.text + '</li>');
-			elem.hide();
-			$('.mainpage-news').prepend(elem);
-			elem.fadeIn('slow');
-		}*/
-
 		if (data.lastn > 0) {
+			// set last notification id
 			lastn = data.lastn;
 		}
 	});
 }, 10000);
 
+/*!
+ * counts all unread notifications and add to counter
+ */
 function updateNotificationsCount() {
 	var unread = $('.notifications-container .unread').size();
 	if (unread > 0) {
+		// change pagetitle
 		$(document).title('reset');
 		$(document).title('prepend', '(' + unread + ') ');
 		$('.notifications .label').addClass('important');
 	} else {
+		// reset pagetitle, no unread notifications
 		$(document).title('reset');
 		$('.notifications .label').removeClass('important');
 	}
+	
+	
 	$('.notifications .label').html(unread);
 }
 
-/**
- * renders notifications container (remove notifications
+/*!
+ * renders notifications container (removes notifications
  * to fit container to screen)
  */
 function notificationRenderer() {
+	// real height
 	var height = $(window).height() - 20 - $('.notifications-container').offset().top;
 
 	var elemheight = 0;
+	
+	// count height from all elements in container
 	$('.notifications-container .notification, .notifications-container h6').each(function() {
 		if ($(this).height() + elemheight > height) {
 			if ($(this).find('.unread').size() == 0) {
+				// hide element which has no place
 				$(this).hide();
 			}
 		} else {
+			// element fits to container
 			elemheight += $(this).height();
 		}
 	});
 	
-	console.log(height + ' ' + elemheight);
-
+	// fix height
 	$('.notifications-container').css('height', elemheight);
 }
 
+/*!
+ * called if user clicks on notifications link
+ */
 function shownotifications() {
 	if (!$('.notifications').hasClass('notifications-active')) {
-		 $('.notifications').addClass('notifications-active');
-		 $('.notifications-container').show();
+		// show notifications
+		$('.notifications').addClass('notifications-active');
+		$('.notifications-container').show();
 		 
-		 notificationRenderer();
+		notificationRenderer();
 	} else {
+		// hide notifications
 		$('.notifications').removeClass('notifications-active');
-		 $('.notifications-container').hide();
-		 
-		 $('.notifications-container .unread').removeClass('unread');
-		 $.get(BASE_URL + 'notifications/?read=1');
-		 updateNotificationsCount();
-		 
+		$('.notifications-container').hide();
+		
+		// change state from all notifications
+		$('.notifications-container .unread').removeClass('unread');
+		$.get(BASE_URL + 'notifications/?read=1');
+		
+		updateNotificationsCount(); 
 	}
 }
