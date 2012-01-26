84ddacfd19e6b13af348b4d0a2f38c37fca0f42c
benn0r <admin@benn0r.ch>
2012-01-26 21:08:38 +0100

    removed isBoard stuff

--- application/Helper.php
+++ application/Helper.php
@@ -67,26 +67,9 @@ class Helper extends Application_Helper
 			case 'report':
 				return 'Report';
 			default:
-				if ($this->isBoard($module)) {
-					return 'Board';
-				}
 				return 'Index';
 				
 		}
-	}
-	
-	public function isBoard($module) {
-		$db = $this->_application->getDb();
-		
-		$result = $db->select('SELECT * FROM board_boards
-			WHERE pbid > 0 AND url_alias = "' . $db->escape($module) . '"');
-		
-		if ($result->num_rows > 0) {
-			return true;
-		}
-		
-		// Kein Board
-		return false;
 	}
 	
 }
